e9c8b3547e4db1118c6f850cf4f7f401b24aa7fc
Hasan-new <mdhasan.yahoo@gmail.com>
2021-04-16 20:51:40 +0600

    map done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 
 
 const Shop = () => {
-    fakeData
-    console.log(fakeData);
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
     return (
         <div>
            <h1>This is Shop</h1> 
+           <ul>
+               {
+                   products.map(product => <li>{product.name}</li>)
+               }
+           </ul>
         </div>
     );
 };
