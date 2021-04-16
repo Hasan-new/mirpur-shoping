9eb13257352228d949c4760b312eeb41cec17800
Hasan-new <mdhasan.yahoo@gmail.com>
2021-04-16 20:30:26 +0600

    hi

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+
+
+const Shop = () => {
+    fakeData
+    console.log(fakeData);
+    return (
+        <div>
+           <h1>This is Shop</h1> 
+        </div>
+    );
+};
+
+export default Shop;<h1>This is Shop</h1>
