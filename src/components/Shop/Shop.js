6846fe66636b0be133cd7634de56392512f79178
Hasan-new <mdhasan.yahoo@gmail.com>
2021-04-18 01:52:05 +0600

    product and shop done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import './Shop.css';
 
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
     return (
-        <div>
-           <h1>This is Shop</h1> 
-           <ul>
-               {
-                   products.map(product => <li>{product.name}</li>)
-               }
-           </ul>
+        <div className="shop-container">
+           <div className="product-container">
+                {
+                    products.map(pd => <Product product={pd}></Product>)
+                }
+            </div> 
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
+           
         </div>
     );
 };
